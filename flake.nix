78fa4635f95b883beb6fe4ca9c490c56c1ac2267
David Mohar <david.mohar@shekaj.si>
2023-08-30 14:08:43 +0200

    temp

---
+++ flake.nix
@@ -0,0 +1,11 @@
+{
+  description = "My NixOS Configuration & dotfiles";
+
+  inputs = {
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/*.tar.gz";
+  };
+
+  outputs = { self, nixpkgs }: {
+
+  };
+}
